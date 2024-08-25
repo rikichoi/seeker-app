2b868565b9dd12bb13940f37f09be7b77f327455
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-08-26 00:51:28 +1000

    added try again button to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,11 +1,19 @@
 "use client";
 import React from "react";
 
-export default function Error() {
+type ErrorProps = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function Error({ error, reset }: ErrorProps) {
   return (
     <div className="min-h-[70vh]">
       <h1 className="text-5xl">Oops, there was an error!</h1>
       <p className="text-2xl">Please try again</p>
+      <button onClick={reset} className="btn btn-primary">
+        Try again
+      </button>
     </div>
   );
 }
