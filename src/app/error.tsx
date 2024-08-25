43043d8ecf2a8f926c7de21ea746c30d220aad25
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-08-26 00:44:20 +1000

    uppdated error and notfound pages

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function Error() {
+  return (
+    <div>
+      <h1 className="text-5xl">Oops, there was an erro!</h1>
+      <p className="text-2xl">Please try again</p>
+    </div>
+  );
+}
--- src/app/notfound.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotFoundPage() {
-  return (
-    <div>Job Posting Not Found.</div>
-  )
-}
